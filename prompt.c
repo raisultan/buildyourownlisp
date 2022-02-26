9109b54bcd4e8abc397586a26a8d6e9bfb63da9b
Raisultan Karimov <ki.xbozz@gmail.com>
2022-02-27 05:23:10 +0700

    eval

--- prompt.c
+++ prompt.c
@@ -28,7 +28,32 @@ void add_history(char* unused) {}
 
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if (!strcmp(op, "+")) { return x + y; }
+  if (!strcmp(op, "-")) { return x - y; }
+  if (!strcmp(op, "*")) { return x * y; }
+  if (!strcmp(op, "/")) { return x / y; }
+  return 0;
+}
 
+long eval(mpc_ast_t* t) {
+  // base case
+  if (strstr(t->tag, "number")) { return atoi(t->contents); }
+
+  // the operator is always second child
+  char* op = t->children[1]->contents;
+
+  // store the third child in 'x'
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
@@ -55,6 +80,8 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("result: %li\n\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
