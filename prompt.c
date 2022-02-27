662226332c325a2c624b8471d8e0251eb7efc599
Raisultan Karimov <ki.xbozz@gmail.com>
2022-02-27 18:56:32 +0700

    lval struct & print

--- prompt.c
+++ prompt.c
@@ -55,6 +55,45 @@ long eval(mpc_ast_t* t) {
   return x;
 }
 
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+    case LVAL_NUM: printf("%li", v.num); break;
+    case LVAL_ERR:
+      if (v.err == LERR_DIV_ZERO) { printf("Error: Division By Zero!"); }
+      if (v.err == LERR_BAD_OP) { printf("Error: Invalid Operator!"); }
+      if (v.err == LERR_BAD_NUM) { printf("Error: Invalid Number!"); }
+    break;
+  }
+}
+
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+// type field
+enum { LVAL_NUM, LVAL_ERR };  // automatically assigned integers
+
+// err field
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
