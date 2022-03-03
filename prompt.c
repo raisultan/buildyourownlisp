d5f27f0dbef5e94a280d04637807d7746c581f36
Raisultan Karimov <ki.xbozz@gmail.com>
2022-03-04 04:46:25 +0700

    join

--- prompt.c
+++ prompt.c
@@ -310,6 +310,32 @@ lval* builtin_eval(lval* a) {
   return lval_eval(x);
 }
 
+lval* builtin_join(lval* a) {
+  for (int i = 0; i < a->count; i++) {
+    LASSERT(a, a->cell[i]->type == LVAL_QEXPR, "Function 'join' passed incorrect type!");
+  }
+
+  lval* x = lval_pop(a, 0);
+
+  while (a->count) {
+    x = lval_join(x, lval_pop(a, 0));
+  }
+
+  lval_del(a);
+  return x;
+}
+
+lval* lval_join(lval* x, lval* y) {
+  // for each cell in y add it to x
+  while (y->count) {
+    x = lval_add(x, lval_pop(y, 0));
+  }
+
+  // y is empty, so delete it
+  lval_del(y);
+  return x;
+}
+
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Symbol = mpc_new("symbol");
